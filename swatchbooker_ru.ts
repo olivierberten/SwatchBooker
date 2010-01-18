68ab27f16494b77f3a84e3afb11258c8a13e5df0
Olivier Berten <olivier.berten@gmail.com>
2010-01-18 14:41:56 +0000

    Added russian translation (thanks to Alexandre Prokoudine)

---
+++ swatchbooker_ru.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="ru_RU">
+<context>
+    <name>ColorWidget</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="105"/>
+        <source>Color</source>
+        <translation>Цвет</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="107"/>
+        <source>Name:</source>
+        <translation>Название:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="109"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="113"/>
+        <source>Spot</source>
+        <translation>Плашечный</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="143"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="144"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="138"/>
+        <source>Key</source>
+        <translation>Ключ</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="138"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="149"/>
+        <source>Extra info</source>
+        <translation>Дополнительная информация</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="266"/>
+        <source>Profile</source>
+        <translation>Профиль</translation>
+    </message>
+</context>
+<context>
+    <name>GroupWidget</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="56"/>
+        <source>Group</source>
+        <translation>Группа</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="58"/>
+        <source>Name:</source>
+        <translation>Название:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="60"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="437"/>
+        <source>Name:</source>
+        <translation>Название:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="439"/>
+        <source>Description:</source>
+        <translation>Описание:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="441"/>
+        <source>Copyright:</source>
+        <translation>Авторские права:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="443"/>
+        <source>Version:</source>
+        <translation>Версия:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="445"/>
+        <source>License:</source>
+        <translation>Лицензия:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="448"/>
+        <source>Information</source>
+        <translation>Информация</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="472"/>
+        <source>Add</source>
+        <translation>Добавить</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="474"/>
+        <source>Remove</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="480"/>
+        <source>Color profiles</source>
+        <translation>Цветовые профили</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="504"/>
+        <source>Add Color</source>
+        <translation>Добавить цвет</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="505"/>
+        <source>Add Gradient</source>
+        <translation>Добавить градиент</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="506"/>
+        <source>Add Pattern</source>
+        <translation>Добавить узор</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="507"/>
+        <source>Add Texture</source>
+        <translation>Добавить текстуру</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="511"/>
+        <source>Add Spacer</source>
+        <translation>Добавить интервал</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="512"/>
+        <source>Add Break</source>
+        <translation>Добавить разделитель</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="514"/>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="519"/>
+        <source>Tree view</source>
+        <translation>В виде дерева</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="531"/>
+        <source>Columns:</source>
+        <translation>Столбцов:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="534"/>
+        <source>Rows:</source>
+        <translation>Строк:</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="538"/>
+        <source>Grid view</source>
+        <translation>В виде сетки</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="555"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="556"/>
+        <source>Settings</source>
+        <translation>Параметры</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="557"/>
+        <source>&amp;About</source>
+        <translation>&amp;О программе</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1223"/>
+        <source>SwatchBooker - Choose file</source>
+        <translation>Выберите файл</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="690"/>
+        <source>All supported files (%s)</source>
+        <translation>Все поддерживаемые файлы (%s)</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="690"/>
+        <source>;;All files (*)</source>
+        <translation>;;Все файлы (*)</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="795"/>
+        <source>SwatchBooker - Save file</source>
+        <translation>SwatchBooker — Сохранить файл</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="894"/>
+        <source>About SwatchBooker</source>
+        <translation>О SwatchBooker</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1223"/>
+        <source>ICC profiles (*.icc *.icm)</source>
+        <translation>Профили ICC (*.icc *.icm)</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1303"/>
+        <source>Open from web</source>
+        <translation>Открыть из Сети</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1304"/>
+        <source>&amp;Save As...</source>
+        <translation>Сохранить &amp;как...</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1301"/>
+        <source>&amp;New</source>
+        <translation>&amp;Создать</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1302"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="509"/>
+        <source>Add Group</source>
+        <translation>Добавить группу</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1212"/>
+        <source>swatches</source>
+        <translation>образцов</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1214"/>
+        <source>swatch</source>
+        <translation>образец</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDlg</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="1463"/>
+        <source>Choose file</source>
+        <translation>Указать файл</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1343"/>
+        <source>Display Profile</source>
+        <translation>Профиль монитора</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1358"/>
+        <source>Default CMYK Profile</source>
+        <translation>Профиль CMYK по умолчанию</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1405"/>
+        <source>SwatchBooker - Settings</source>
+        <translation>Параметры SwatchBooker</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1457"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1412"/>
+        <source>You must choose either sRGB or a file</source>
+        <translation>Необходимо выбрать или sRGB, или файл профиля</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1418"/>
+        <source>You must choose either Fogra27L or a file</source>
+        <translation>Необходимо выбрать или Fogra27L, или файл профиля</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1463"/>
+        <source>ICC profiles (*.icc *.icm)</source>
+        <translation>Профили ICC (*.icc *.icm)</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1459"/>
+        <source>This isn&apos;t a RGB monitor profile</source>
+        <translation>Это не профиль RGB для монитора</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1474"/>
+        <source>This isn&apos;t a CMYK profile</source>
+        <translation>Это не профиль CMYK</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1367"/>
+        <source>Recent files</source>
+        <translation>Недавние файлы</translation>
+    </message>
+    <message>
+        <location filename="swatchbooker.pyw" line="1369"/>
+        <source>Number of files displayed:</source>
+        <translation>Число отображаемых файлов:</translation>
+    </message>
+</context>
+<context>
+    <name>noChild</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="1323"/>
+        <source>empty</source>
+        <translation>пусто</translation>
+    </message>
+</context>
+<context>
+    <name>webOpenDlg</name>
+    <message>
+        <location filename="swatchbooker.pyw" line="1509"/>
+        <source>SwatchBooker - Open from web</source>
+        <translation>Открыть из Сети</translation>
+    </message>
+</context>
+</TS>
